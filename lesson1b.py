afcc5c4010cdbfa5227c3ad56b588dcd9188b019
Jessica <jmmwikali936@gmail.com>
2026-02-04 13:22:47 +0300

    updates

---
+++ lesson1b.py
@@ -0,0 +1,19 @@
+# Python Data Types
+
+# A string: It is a series of characters thst are inside a quote
+name = "Jessica Mwikali"
+print(name)
+print(type(name))
+
+# Numeric
+# Int - This indictes a whole number in python. For an integer we don't use quotes
+age = 39
+print("The age of a person is:", age)
+print(type(age))
+
+# float - This is a number with a decimal
+coordinates = 15.4524
+print("The coordinates are", coordinates)
+print(type(coordinates))
+
+# Research and come up with a complex data 
