7c0121c7e01a1f8bf5fb3c285c30eaf12153d09a
Jessica <jmmwikali936@gmail.com>
2026-02-04 13:58:52 +0300

    updates

---
+++ lesson1c.py
@@ -0,0 +1,40 @@
+# Arithimetic Operations
+
+# Addition
+number1 = 10
+number2 = 20
+addition = number1 + number2
+print("The sum of the two numbers is:", addition)
+
+# Subtraction
+number3 = 50
+number4 = 15
+difference = number3 - number4
+print("The difference of the numbers is:", difference)
+
+# Multiplication
+number5 = 10
+number6 = 6
+product = number5 * number6
+print("The product of the numbers is:", product)
+
+# Division
+number7 = 50
+number8 = 5
+division = number7 / number8
+print("The division of the numbers is:", division)
+
+# Modulus - This means the remainder of a number when it gets subdivided by another one
+number9 = 14
+number10 = 5
+modulus = number9 % number10
+print("The modulus of the number is:", modulus)
+
+# Floor Division (//) - This is the number of times a number subdivides another one,  ignoring the remainder.
+floor = number9 // number10
+print("The floor division of the two numbers is:", floor)
+
+# Exponent (**) - It is the power of a number
+number11 = 10
+exponent = number11 ** 2
+print("The exponent of the number is:", exponent)
