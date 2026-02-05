6ea3454502ccca5c31cc201566df4779bcf10c37
Jessica <jmmwikali936@gmail.com>
2026-02-05 20:32:06 +0300

    assignment

---
+++ assignment2.py
@@ -0,0 +1,9 @@
+# Tuple of eight planets
+
+planets = ("Mercury", "Venus", "Earth", "Mars", "Jupiter", "Saturn", "Uranus", "Neptune")
+
+# Print all the planets
+print("All the planets in the tuple:", planets)
+
+# Print the planets from index 2 to 6
+print("Planets from index 2 to 6:", planets[2:7])
