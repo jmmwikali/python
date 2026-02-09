e4af0fe9d9b9a0653d8b7833826fd5cc34f123bf
Jessica <jmmwikali936@gmail.com>
2026-02-09 23:40:56 +0300

    updates

--- lesson4task3.py
+++ lesson4task3.py
@@ -3,3 +3,29 @@
 Colors = ["Blue", "Green", "Red", "Pink" , "Black"]
 for list in Colors:
     print(list)
+
+print("=====================================================")
+
+# Research on python functions. Both with parameters and without parameters
+
+# Function without parameters - they do a task without needing any input
+def greet():
+    print("Hello")
+
+greet()
+
+# Function with parameters - they recieve input and change behaviour based on it.
+def greeting(name):
+    print("Hello", name)
+
+greeting("Jessica")
+
+def add(x, y):
+    print(x + y)
+
+add(5,10)
+
+def multiply(a, b):
+    print(a * b)
+
+multiply(6, 5)
