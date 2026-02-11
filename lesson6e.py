fd88c45add2a2c031bfd1102dd1667fb01a65962
Jessica <jmmwikali936@gmail.com>
2026-02-11 13:23:00 +0300

    updates

--- lesson6e.py
+++ lesson6e.py
@@ -1,4 +1,6 @@
 # On the try and except block: You run some codes/statements and if it is successful, the try block will get executed. Otherwise the except block will be executed when there is an anticipated error.
+
+# Division by zero error
 try:
     number = 100 
     answer = number / 0
@@ -6,14 +8,24 @@ try:
 except Exception as e:
     print(f"There is an error: {e}")
 
+# Type error
 try:
     result = "5" + 3
     print(result) 
 except Exception as n:
     print(f"There is an error: {n}")
 
+# Value error
 try:
     num = int("Name") 
     print(num)
 except Exception as x:
     print(f"There is an error: {x}")
+
+# Index error
+try:
+    lst = [1, 2, 3]
+    print(lst[5])
+except Exception as b:
+    print(f"There is an error: {b}")
+    
